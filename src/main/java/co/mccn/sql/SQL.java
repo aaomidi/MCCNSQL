48ac660b16f26c485cef6834afd4b7483a986018
aaomidi <aa_omidi@yahoo.com>
2013-12-22 20:24:32 +0330

    Initial Commit

---
+++ src/main/java/co/mccn/sql/SQL.java
@@ -0,0 +1,62 @@
+package co.mccn.sql;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+
+public class SQL extends JavaPlugin {
+    private Database database;
+
+    public final void onLoad() {
+        if (!new File(this.getDataFolder(), "config.yml").exists()) {
+            this.saveDefaultConfig();
+            this.getLogger().severe("THIS IS NOT A PLUGIN. THIS IS AN API FOR OTHER PLUGINS TO USE. DO NOT EXPECT THIS PLUGIN TO DO ANYTHING.");
+        }
+    }
+
+    public final void onEnable() {
+        try {
+            this.database = new Database(this);
+        } catch (SQLException ex) {
+            this.getLogger().severe("Error connecting to database! Disabling...");
+            ex.printStackTrace();
+            this.getPluginLoader().disablePlugin(this);
+            return;
+        }
+
+    }
+
+    public final void onDisable() {
+        this.database.disconnect();
+    }
+
+    public void executeUpdate(String query, Object... parameters) {
+        this.database.executeUpdate(query, parameters);
+
+    }
+
+    public ResultSet executeQuery(String query, Object... parameters) {
+        return this.database.executeQuery(query, parameters);
+    }
+
+    public void reconnect() {
+        try {
+            this.database.reconnect();
+        } catch (SQLException ex) {
+            getLogger().log(Level.SEVERE, ex.getMessage());
+        }
+    }
+
+    public void disconnect() {
+        this.database.disconnect();
+    }
+
+    public void buildAndFetchColumn(String query, String result, Object... parameters) {
+        this.buildAndFetchColumn(query, result, parameters);
+    }
+
+
+}
